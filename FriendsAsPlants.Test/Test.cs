d502c227552cace257cead8e13e843ac3c3211c7
aufty <theaufty@gmail.com>
2018-10-01 19:15:07 -0700

    Android project added

---
+++ FriendsAsPlants.Test/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace FriendsAsPlants.Test
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
