e1a826c4ae743c00d7c50e482b54eb949ebf564b
aufty <theaufty@gmail.com>
2018-10-01 19:39:19 -0700

    Added base presenter

---
+++ FriendsAsPlants.Presentation/BasePresenter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FriendsAsPlants.Presentation
+{
+    public class BasePresenter
+    {
+        public virtual void OnAttached() { }
+        public virtual void OnDetached() { }
+        public virtual void OnVisible() { }
+        public virtual void OnInvisible() { }
+    }
+}
